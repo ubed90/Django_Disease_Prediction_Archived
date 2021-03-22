afc0ea774f4c11d4081a73f87d0b11f75c9288ee
UbedS <shaikhobaid123@gmail.com>
2021-03-22 17:20:00 +0530

    GETTING All MODELS PREDICTION

--- shdps/main_app/views.py
+++ shdps/main_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
 from datetime import date
-
+import os , re
+import pandas as pd
+import joblib as jb
+from sklearn.preprocessing import LabelEncoder
 from django.contrib import messages
 from django.contrib.auth.models import User , auth
 from .models import patient , doctor , diseaseinfo , consultation ,rating_review
@@ -11,11 +14,70 @@ from chats.models import Chat,Feedback
 # Create your views here.
 
 
+# Loading DataSet
+encoder = LabelEncoder()
+dirs = os.listdir('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Datasets')
+dirs = dirs[2]
+num = int(re.findall('[0-9]+' , dirs)[0])
+headers = [*pd.read_csv(f'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Datasets\SHDPS_Training_{num}.csv', nrows=1)]
+dataset = pd.read_csv(f'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Datasets\SHDPS_Training_{num}.csv', usecols=[c for c in headers if c != 'Unnamed: 0'])
+
 #loading model
-import joblib as jb
-model = jb.load(r'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\shdps\ModelAI\model')
+# model = jb.load(r'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\shdps\ModelAI\model')
+
+def get_disease(model , user_symptoms , predicted , Y_train , Y):
+   for num , disease in zip(Y_train , Y):
+            if int(num) == int(predicted[0]):  
+               predicted = disease
+               cs = model.predict_proba(user_symptoms)
+               cs = cs.max()*100
+               return predicted , cs
+   else:
+      return None , None
 
 
+def get_disease_with_max_accuracy(user_symptoms , Y_train , Y):
+   disease_and_Cs = []
+   dt_model = jb.load('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Decision_Tree\Decision_Tree_Model.sav')
+   knn_model = jb.load('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\K_Nearest_Neighbors\KNN_Model.sav')
+   svm_model = jb.load('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Kernel_SVM\Kernel_SVM_Model.sav')
+   lr_model = jb.load('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Logistic_Regression\Logistic_Regression_Model.sav')
+   gaussian_NB_model = jb.load(r'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Naive_Bayes\Gaussian_Bayes_Model.sav')
+   multinomial_NB_model = jb.load(r'E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Naive_Bayes\Multinomial_Bayes_Model.sav')
+   rf_model = jb.load('E:\PROGRAMMING\Projects\Django_Disease_Prediction_System\SHDPS_MODEL_SELECTION\Random_Forest\Random_Forest_Model.sav')
+
+   dt_model_predicted = dt_model.predict(user_symptoms)
+   dt_model_predicted_disease , dt_model_predicted_CS = get_disease(dt_model , user_symptoms , dt_model_predicted , Y_train , Y)
+   disease_and_Cs.append((dt_model_predicted_disease , dt_model_predicted_CS))
+
+   knn_model_predicted = knn_model.predict(user_symptoms)
+   knn_model_predicted_disease , knn_model_predicted_CS = get_disease(knn_model , user_symptoms , knn_model_predicted , Y_train , Y)
+   disease_and_Cs.append((knn_model_predicted_disease , knn_model_predicted_CS))
+
+   svm_model_predicted = svm_model.predict(user_symptoms)
+   svm_model_predicted_disease , svm_model_predicted_CS = get_disease(svm_model , user_symptoms , svm_model_predicted , Y_train , Y)
+   disease_and_Cs.append((svm_model_predicted_disease , svm_model_predicted_CS))
+
+   lr_model_predicted = lr_model.predict(user_symptoms)
+   lr_model_predicted_disease , lr_model_predicted_CS = get_disease(lr_model , user_symptoms , lr_model_predicted , Y_train , Y)
+   disease_and_Cs.append((lr_model_predicted_disease , lr_model_predicted_CS))
+
+   gaussian_NB_model_predicted = gaussian_NB_model.predict(user_symptoms)
+   gaussian_NB_model_predicted_disease , gaussian_NB_model_predicted_CS = get_disease(gaussian_NB_model , user_symptoms , gaussian_NB_model_predicted , Y_train , Y)
+   disease_and_Cs.append((gaussian_NB_model_predicted_disease , gaussian_NB_model_predicted_CS))
+
+   multinomial_NB_model_predicted = multinomial_NB_model.predict(user_symptoms)
+   multinomial_NB_model_predicted_disease , multinomial_NB_model_predicted_CS = get_disease(multinomial_NB_model , user_symptoms , multinomial_NB_model_predicted , Y_train , Y)
+   disease_and_Cs.append((multinomial_NB_model_predicted_disease , multinomial_NB_model_predicted_CS))
+
+   rf_model_predicted = rf_model.predict(user_symptoms)
+   rf_model_predicted_disease , rf_model_predicted_CS = get_disease(rf_model , user_symptoms , rf_model_predicted , Y_train , Y)
+   disease_and_Cs.append((rf_model_predicted_disease , rf_model_predicted_CS))
+
+   disease_and_Cs.sort(key = lambda x: x[1] , reverse=True)
+   return disease_and_Cs[0][0] , disease_and_Cs[0][1]
+
+   
 
 
 def home(request):
@@ -99,40 +161,10 @@ def pviewprofile(request, patientusername):
 
 
 def checkdisease(request):
+  Y = dataset.iloc[ : , 0].values
+  Y_train = encoder.fit_transform(Y)
 
-  diseaselist=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction','Peptic ulcer diseae','AIDS','Diabetes ',
-  'Gastroenteritis','Bronchial Asthma','Hypertension ','Migraine','Cervical spondylosis','Paralysis (brain hemorrhage)',
-  'Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Hepatitis D',
-  'Hepatitis E', 'Alcoholic hepatitis','Tuberculosis', 'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
-  'Heart attack', 'Varicose veins','Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
-  'Arthritis', '(vertigo) Paroymsal  Positional Vertigo','Acne', 'Urinary tract infection', 'Psoriasis', 'Impetigo']
-
-
-  symptomslist=['itching','skin_rash','nodal_skin_eruptions','continuous_sneezing','shivering','chills','joint_pain',
-  'stomach_pain','acidity','ulcers_on_tongue','muscle_wasting','vomiting','burning_micturition','spotting_ urination',
-  'fatigue','weight_gain','anxiety','cold_hands_and_feets','mood_swings','weight_loss','restlessness','lethargy',
-  'patches_in_throat','irregular_sugar_level','cough','high_fever','sunken_eyes','breathlessness','sweating',
-  'dehydration','indigestion','headache','yellowish_skin','dark_urine','nausea','loss_of_appetite','pain_behind_the_eyes',
-  'back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-  'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-  'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-  'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-  'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-  'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-  'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-  'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-  'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-  'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-  'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-  'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-  'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-  'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-  'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-  'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-  'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-  'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-  'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-  'yellow_crust_ooze']
+  symptomslist = list(dataset.columns[1 : ])
 
   alphabaticsymptomslist = sorted(symptomslist)
 
@@ -186,17 +218,13 @@ def checkdisease(request):
 
         print(inputtest)
       
-
-        predicted = model.predict(inputtest)
+        predicted , confidencescore = get_disease_with_max_accuracy(inputtest , Y_train , Y)
         print("predicted disease is : ")
         print(predicted)
 
-        y_pred_2 = model.predict_proba(inputtest)
-        confidencescore=y_pred_2.max() * 100
-        print(" confidence score of : = {0} ".format(confidencescore))
-
         confidencescore = format(confidencescore, '.0f')
-        predicted_disease = predicted[0]
+        print(confidencescore)
+        predicted_disease = predicted
 
         
 
